c7d4607ac107fbce87c72c2306671e3be9ccc62b
noeru_desu <3203911711@qq.com>
2022-01-14 16:36:02 +0800

    Update constants.py

--- real_esrgan_gui/constants.py
+++ real_esrgan_gui/constants.py
@@ -2,7 +2,7 @@
 Author       : noeru_desu
 Date         : 2021-12-19 16:50:59
 LastEditors  : noeru_desu
-LastEditTime : 2022-01-14 16:21:52
+LastEditTime : 2022-01-14 16:35:55
 Description  : 常量
 '''
 # 标志
@@ -19,9 +19,9 @@ BETA = 12
 ALPHA = 13
 VERSION_TYPE = RELEASE
 
-VERSION_NUMBER = '0.2.0'
+VERSION_NUMBER = '0.2.1'
 SUB_VERSION_NUMBER = 'release'
-VERSION_BATCH = '20220114-2'
+VERSION_BATCH = '20220114-3'
 
 BRANCH = 'dev/0.2.x'
 OPEN_SOURCE_URL = 'https://github.com/noeru-desu/Real-ESRGAN-GUI'
